59214084654a3eda7b73b5bb5b72672cdfaafcbb
BicycleChrist <135588674+BicycleChrist@users.noreply.github.com>
2023-07-19 21:03:25 -0800

    Add files via upload

--- EffortInterface.py
+++ EffortInterface.py
@@ -1,20 +1,37 @@
 import tkinter as gui
+from tkinter import StringVar
+from tkinter.ttk import OptionMenu
+import json
+import pathlib
 from FinanceFuckAround import *
 
 def retrieve_and_display_data():
     ticker = ticker_entry.get()  # Get the ticker from the entry field
-    statement_type = statement_type_entry.get()  # Get the statement type from the entry field
+    statement_type = statement_type_var.get()  # Get the selected statement type from the dropdown
 
     # Download data if it doesn't exist already
     filename = GetFilename(ticker, statement_type)
     if not pathlib.Path(filename).exists():
-        DownloadFile(ticker, statement_type)
+        data = DownloadFile(ticker, statement_type)
+    else:
+        # Load the data if it exists
+        with open(filename) as file:
+            data = json.load(file)
 
-    # Load the data and display it in the text widget
-    with open(filename) as file:
-        data = json.load(file)
-        data_text.delete(1.0, gui.END)  # Clear any existing data in the text widget
-        data_text.insert(gui.END, json.dumps(data, indent=4))
+    # Display the data in the text widget
+    data_text.delete(1.0, gui.END)  # Clear any existing data in the text widget
+    data_text.insert(gui.END, json.dumps(data, indent=4))
+
+        
+        
+        
+        
+def download_data():
+    ticker = download_ticker_entry.get()
+    statement_type= download_statement_type_entry.get()
+    
+    DownloadFile(ticker,statement_type)
+
 
 app = gui.Tk()
 app.title("Visualized Effort")
@@ -28,13 +45,15 @@ ticker_entry.pack()
 statement_type_label = gui.Label(app, text="Enter Statement Type:")
 statement_type_label.pack()
 
-statement_type_entry = gui.Entry(app)
-statement_type_entry.pack()
+statement_type_var = StringVar(app)
+statement_type_var.set("INCOME_STATEMENT")  # Default value for the dropdown
+statement_type_menu = OptionMenu(app, statement_type_var, "INCOME_STATEMENT", "BALANCE_SHEET", "CASH_FLOW")
+statement_type_menu.pack()
 
 retrieve_button = gui.Button(app, text="Retrieve and Display Data", command=retrieve_and_display_data)
 retrieve_button.pack()
 
-data_text = gui.Text(app, width=80, height=50)
+data_text = gui.Text(app, width=100, height=50)
 data_text.pack()
 
 app.mainloop()
--- FinanceFuckAround.py
+++ FinanceFuckAround.py
@@ -84,7 +84,7 @@ def MoveJsonFiles():
 
 def DownloadFile(ticker=default_ticker, statement_type=default_statementtype):
     url = "https://www.alphavantage.co/query"
-    headers = {"X-RapidAPI-Key": "0000000", "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com"}
+    headers = {"X-RapidAPI-Key": "", "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com"}
     params={"apikey": "","X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com","symbol":ticker,"function":statement_type,"interval":"5min","output_size":"full","datatype":".csv"} # should have timeout
     response = requests.get(url, headers=headers, params=params)
     
