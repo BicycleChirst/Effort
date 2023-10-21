ed9592c698fdbb08c6bb91f677f05aabd522be1d
DADMIN <139256274+DADMIN1@users.noreply.github.com>
2023-10-21 12:25:29 -0800

    implemented loading files based on GUI selections with detection for missing files
    prototyped code to gather data for graphing
    
    added some info on matplotlib backend

--- GraphingWithDADMIN.py
+++ GraphingWithDADMIN.py
@@ -1,8 +1,34 @@
 import tkinter
 from JSONwithDADMIN import *
 
+import matplotlib
 from matplotlib import pyplot
 from matplotlib.ticker import FuncFormatter
+
+# for some reason importing pyplot changes the behavior of Tk in Pycharm's console;
+# it prints a message:
+# 'Backend TkAgg is interactive backend. Turning interactive mode on.'
+# and then immediately opens the tkWindow if/when it's defined
+
+# normal python3/ipython is using 'QtAgg'
+# for some reason ipython also prints: 'Installed qt5 event loop hook.'
+# interactive console uses 'TkAgg'
+
+# matplotlib.use('QtAgg', force=True)
+print(f"matplotlib backend is using: {matplotlib.get_backend()}")
+print(f"""interactive status:
+    matplotlib: {matplotlib.is_interactive()}
+    pyplot: {pyplot.isinteractive()}
+""")
+
+# backend supported values are:
+# [ 'GTK3Agg', 'GTK3Cairo', 'GTK4Agg', 'GTK4Cairo',
+# 'MacOSX', 'nbAgg',
+# 'QtAgg', 'QtCairo', 'Qt5Agg', 'Qt5Cairo',
+# 'TkAgg', 'TkCairo',
+# 'WebAgg',
+# 'WX', 'WXAgg', 'WXCairo',
+# 'agg', 'cairo', 'pdf', 'pgf', 'ps', 'svg', 'template' ]
 
 
 helptext = """
@@ -69,29 +95,60 @@ tkinter.Button(topframe, text=f"GRAPH", command=AllCallbacks).pack(fill="both", 
 
 
 # 'pos' appears to be a magic variable
-def y_axis_formatter(x, pos, currency='$'):
-    if x == 0: return '0'
-    abs_x = abs(x)
-    if abs_x >= 1e9:
-        formatted_x = f'{abs_x / 1e9:.1f}B'
+def y_axis_formatter(y, pos, currency='$'):
+    if y == 0: return '0'
+    abs_y = abs(y)
+    if abs_y >= 1e9:
+        formatted_y = f'{abs_y / 1e9:.1f}B'
     else:
-        formatted_x = f'{abs_x / 1e6:.1f}M'
-    formatted_x = currency + formatted_x
+        formatted_y = f'{abs_y / 1e6:.1f}M'
+    formatted_y = currency + formatted_y
     # Add a minus sign for negative values
-    if x < 0: formatted_x = '-' + formatted_x
-    return formatted_x
-# TODO: store currency-symbol in a global variable or bind it in a lambda
+    if y < 0: formatted_y = '-' + formatted_y
+    return formatted_y
+# TODO: store currency-symbol in a global variable or bind it in a lambda or something
 
 
-def LoadUserSelections():
-    plot_ticker_var = 'AMD'
-    plot_statementtype_var = 'INCOME_STATEMENT'
-    selectedfiles = []
-    file = LOADED_FILES[f"{plot_ticker_var}_{plot_statementtype_var}"]
-    # convert all fields to floats, get rid of 'None's, reverse order of dates
-    file = ConvertJSONnumbers(file, True)
-    selectedfiles.append(file)
-    return selectedfiles
+def LoadSelectedFiles():
+    if not tickerbox_selections:  # if empty
+        print("Error: No Tickers selected")
+        return
+    WantedKeys.update(keybox_selections)
+    # ^ this only works because keybox_selections puts an empty list in the field
+    # ('update' does not delete/empty non-existent keys)
+    # can't be iterating through WantedKeys when you delete
+    for (KK, KL) in keybox_selections.items():
+        if not KL:  # empty list
+            del WantedKeys[KK]
+    if not WantedKeys:
+        print("Error: No fields selected")
+        return
+
+    # {Ticker : [json-objects]}
+    SelectedFilemap = {}
+    for ticker in tickerbox_selections:
+        SelectedFilemap[ticker] = []
+        print(f"loading statements for: {ticker}")
+        for statementtype in WantedKeys.keys():
+            if statementtype not in StatementMap[ticker]:
+                print(f"\tWARNING: skipping {statementtype}; file does not exist")
+                continue
+            print(f"\tloading {statementtype}")
+            file = LOADED_FILES[f"{ticker}_{statementtype}"]
+            file = ConvertJSONnumbers(file, True)
+            # ^ convert all fields to floats, get rid of 'None's, reverse order of dates
+            SelectedFilemap[ticker].append(file)
+        # removing ticker if no files were loaded for it
+        if not SelectedFilemap[ticker]:
+            del SelectedFilemap[ticker]
+
+    print("loaded all Selected Files")
+    return SelectedFilemap
+
+
+# TODO: figure out if annualReports are purely redundant (always overlap with quarterly)
+# TODO: figure out if duplicate keys across statement-types are equivalent
+    # like 'netIncome' in both 'INCOME_STATEMENT' and 'CASH_FLOW'
 
 
 def PlotWantedKeys(thejson, figure=None):
@@ -118,15 +175,17 @@ def PlotWantedKeys(thejson, figure=None):
     return figure
 
 
+# TODO: pull out all the data here
 # we'll add this function to the callbacks
 def Graph():
     # assume that all other callbacks have triggered
-    userselection = LoadUserSelections()
-    newfig = None
-    for F in userselection:
-        newfig = PlotWantedKeys(F)
-    PlotWantedKeys(userselection[0])
-    pyplot.show(block=False)
+    filemap = LoadSelectedFiles()
+    #newfig = None
+    #for F in userselection:
+    #    newfig = PlotWantedKeys(F)
+    #PlotWantedKeys(userselection)
+    #pyplot.show(block=False)
+
 
 # instead of calling '.show' without blocking,
 # you can instead enable "interactive mode", which automatically shows figures
@@ -134,12 +193,39 @@ def Graph():
 #pyplot.ion()
 # do I call this before the other pyplot stuff or what?
 
+# qr = userselections['IBM'][0]['quarterlyReports']
+# ziptest = list(list(zip([r['fiscalDateEnding'], r['grossProfit'], r['reportedCurrency']])) for r in qr)
+
 if __name__ == '__main__':
-    CallbackList.append(Graph)
-    tkWindow.mainloop()
+    #CallbackList.append(Graph)
+    #tkWindow.mainloop()
+    tickerbox_selections = ["AMD", "INTL"]
+    keybox_selections = {
+        "INCOME_STATEMENT": ["grossProfit", "netIncome"],
+        "BALANCE_SHEET": ["longTermDebt", "totalAssets"],
+        "CASH_FLOW": [],  # this will always have all entries, with blank lists if no selection
+    }
+    filemap = LoadSelectedFiles()
+    
+    extracted_data = {}
+    amdfiles = filemap['AMD']
+    thestatement = amdfiles[0]
+    targetkeys = WantedKeys[thestatement['StatementType']]
+    reps = thestatement['quarterlyReports']
+    
+    thedates = [R['fiscalDateEnding'] for R in reps]
+    extracted_data.update({"dates": thedates})
+    for tk in targetkeys:
+        extracted_data.update({tk: [R[tk] for R in reps]})
+    
+    #thedata = [[R[tk] for R in reps] for tk in targetkeys]
+    #bigcombo = list(zip(reps[0].keys(), reps[0].values(), reps[1].values()))
+    #bigcombo = list(zip(reps[0].keys(), *[R.values() for R in reps]))
+    # if you're using the zip method, it's important that the targetkeys are sorted (or not?)
     print("window closed")
 
-# TODO: check that the selected statementtypes actually exist
+
+# TODO: what if the dates don't match across files?
 # TODO: replace the jank nested-functions with actual lambdas
 # TODO: figure out why passing the 'newfig' creates overlapping axes
 # TODO: implement some kind of config-file to save GUI-selection states
